8687611161377e680f1f9405083df2db37b6cee2
SergeAyer <Serge.Ayer@hefr.ch>
2022-08-17 14:04:18 +0000

    Added main_absolute_value

---
+++ main_absolute_value.cpp
@@ -0,0 +1,30 @@
+#include "mbed.h"
+
+#include "mbed_trace.h"
+#if MBED_CONF_MBED_TRACE_ENABLE
+#define TRACE_GROUP "main"
+#endif // MBED_CONF_MBED_TRACE_ENABLE
+
+inline int absolute_value(int i) { return i >= 0 ? i : -i; }
+
+static int nbrOfCalls = 0;
+
+// f is a function returning an integer value
+int f() 
+{
+    nbrOfCalls++;
+    return -1;  
+}
+
+int main() 
+{    
+    // use trace library for console output
+    mbed_trace_init();
+ 
+    int x = -2;
+    int ans1 = absolute_value(++x); // what is the value of ans1 ?
+    int ans2 = absolute_value(f()); // how many times is f() called?
+
+    tr_debug("value of ans1 is %d", ans1);
+    tr_debug("nbr of calls of f() is %d", nbrOfCalls);
+}
