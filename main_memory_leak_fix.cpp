2261d789819b17c6a529041f6061350d82cf516d
SergeAyer <42145027+SergeAyer@users.noreply.github.com>
2022-08-23 16:34:03 +0200

    Added main_memory_leak_fix

---
+++ main_memory_leak_fix.cpp
@@ -0,0 +1,36 @@
+
+#include "mbed.h"
+
+#include "mbed_trace.h"
+#if MBED_CONF_MBED_TRACE_ENABLE
+#define TRACE_GROUP "main"
+#endif // MBED_CONF_MBED_TRACE_ENABLE
+
+void function_with_memory_leak_fix()
+{
+    // solution with raw pointer
+    char* pArray = new char[100];
+    // for preventing compiler optimization use pArray
+    tr_debug("pArray[100] = %d", pArray[100]);
+    // call delete []    
+    delete [] pArray;
+    pArray = nullptr;
+
+    // solution with unique_ptr 
+    // memory will be automatically released when array_ptr is removed from the stack
+    std::unique_ptr<char> array_ptr = std::unique_ptr<char>(new char[100]);
+    tr_debug("array_ptr[100] = %d", array_ptr.get()[100]);    
+}
+
+int main() 
+{    
+    // use trace library for console output
+    mbed_trace_init();
+
+    for (int i = 0; i < 10000; i++) {
+        function_with_memory_leak_fix();
+        tr_debug("Iteration #%i", i);
+    }
+
+    return 0;
+}
